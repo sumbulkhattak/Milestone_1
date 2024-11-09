6ddddc50dc994a0a487e7dd607886582ee0bf449
sumbulkhattak <misbah.k2326@gmail.com>
2024-11-09 22:55:52 +0500

    HelloWorld

---
+++ app/Components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+        <><div className=" p-6  justify-center text-center w-full flex font-semibold  bg-black  text-white">
+        <ul className=" flex gap-10 text-xl  ">
+            <li><a href="">Home</a></li>
+            <li><a href=""></a>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <div className=" text-center w-100 p-7 h-20  mt-[180px] bg-slate-950 text-white font-bold   ">
+       <p>Hello World!</p>
+        </div>
+        </>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Navbar from "./Components/Header";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+}
